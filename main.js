0540273e43fc13ee55440dcc674c0f6bbdac31b5
razgacheru <roland.viray@yahoo.com>
2023-05-12 21:16:01 +1000

    worked on responsiveness

--- main.js
+++ main.js
@@ -3,17 +3,7 @@ const nav = document.getElementById('nav');
 let submit = document.getElementById('submit');
 
 submit.addEventListener('click', result);
-window.addEventListener('scroll', function() {
-  const currentScrollPos = window.pageYOffset;
-  
-  if (prevScrollPos > currentScrollPos) {
-    nav.classList.remove('hidden');
-  } else {
-    nav.classList.add('hidden');
-  }
-  
-  prevScrollPos = currentScrollPos;
-});
+
 
 function result() {
   alert('Email sent! Thank you for reaching out :)');
